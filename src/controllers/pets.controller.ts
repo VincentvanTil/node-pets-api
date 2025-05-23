6ca9ef8cbb68c93a7f302d46d08399edec673540
Vincent <0948125@hr.nl>
2025-05-23 13:36:59 +0200

    Added tests for abstraction of data layers

--- src/controllers/pets.controller.ts
+++ src/controllers/pets.controller.ts
@@ -9,7 +9,10 @@ export const getAllPets = async (req: Request, res: Response) => {
 export const getPetById = async (req: Request, res: Response) => {
   const id = parseInt(req.params.id, 10);
   const pet = await fetchPetById(id);
+
   if (isNaN(id)) return res.status(400).json({ message: "Invalid ID type" });
+
   if (!pet) return res.status(404).json({ message: "Pet not found" });
+
   res.json(pet);
 };
